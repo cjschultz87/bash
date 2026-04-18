c27e8e5e08433d3424b44811abe68234a31c124d
CJ <131416165+cjschultz87@users.noreply.github.com>
2026-04-18 11:54:39 -0400

    uploaded november1.sh
    modded november.sh to use foxcom as an example of the custom tcp flags option, which can be used with wireshark.

---
+++ november1.sh
@@ -0,0 +1,43 @@
+echo "randomizing common ports.";
+
+romeo=$(/home/john/cc/romeoarray 1 1023 " ");
+
+alpha=($romeo);
+
+aL=${#alpha[*]};
+
+read -p "target ip: " tango;
+
+index=0;
+
+while [ $index -lt $aL ];
+do
+
+sudo ufw insert 1 allow out to $tango port ${alpha[$index]} &>/dev/null;
+
+sudo ufw insert 1 allow in from $tango port ${alpha[$index]} &>/dev/null;
+
+echo "$index/$aL: $tango port ${alpha[$index]}";
+
+sierra="$(($RANDOM%256 * 256**3 + $RANDOM%256 * 256**2 + $RANDOM%256 * 256**1 + $RANDOM%256))";
+
+ack="$(($RANDOM%256 * 256**3 + $RANDOM%256 * 256**2 + $RANDOM%256 * 256**1 + $RANDOM%256))";
+
+sudo python3 ./python/foxcom1.1.py p 80 ${alpha[$index]} type tcp 00000010 172 sa $sierra $ack 10.0.2.7 10.0.2.8 enp0s3 &>/dev/null;
+
+#sleep "$(($RANDOM % 15))s";
+
+index_ufw=0;
+
+while [ $index_ufw -lt 2 ];
+do
+
+echo "y" | sudo ufw delete 1 &>/dev/null;
+
+((index_ufw++));
+
+done;
+
+((index++));
+
+done;
